1ea4ba108d0e0d35bbed320df31d9fe04cf9f97f
LJY <q892000@gmail.com>
2021-11-27 11:22:48 +0800

    Initial Commit

---
+++ ios_00857017_hw3/ios_00857017_hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_00857017_hw3App.swift
+//  ios_00857017_hw3
+//
+//  Created by User14 on 2021/11/27.
+//
+
+import SwiftUI
+
+@main
+struct ios_00857017_hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
